4097322f3815c134a36ca2f3fdfd4615df09bf08
Marcos <markitosanches@gmail.com>
2025-02-27 15:45:55 -0500

    Class 4

---
+++ ecommerce/client/src/components/MainFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="w-full mx-auto border-t border-gray-100 mt-1 mx-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+        <span class="text-sm text-gray-500 sm:text-center">All Rights reserved @2025.</span>
+    </footer>
+</template>
